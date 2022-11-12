90c4181efffec09445fbcd2e8c262925fff4d601
MRXPRO200330020179Y <mrxpro2003@gmail.com>
2022-11-12 16:27:59 +0500

    узгаришлар

--- public/js/index.js
+++ public/js/index.js
@@ -72,7 +72,20 @@ $(document).ready(function(){
     })
   });
 
-
+  const buttons = document.querySelectorAll('.header__link__title');
+  buttons.forEach(function(button, index) {
+    button.addEventListener('click', function(e) {
+      e.preventDefault();
+      
+      this.parentNode.classList.toggle('active');
+      
+      buttons.forEach(function(button2, index2) {
+        if ( index !== index2 ) {
+          button2.parentNode.classList.remove('active');
+        }
+      });
+    });
+  });
 
 
 // --------------------------------
